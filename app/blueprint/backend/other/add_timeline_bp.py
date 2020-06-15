b5254ef0b64b56a77f7f95a012ec8ee3f99ad894
weijiang1994 <804022023@qq.com>
2020-06-15 22:40:42 +0800

    时间线页面小修改

--- app/blueprint/backend/other/add_timeline_bp.py
+++ app/blueprint/backend/other/add_timeline_bp.py
@@ -8,14 +8,21 @@ file: add_timeline_bp.py
 """
 from flask import Blueprint, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField, SubmitField
 
 add_timeline_bp = Blueprint('add_timeline_bp', __name__, url_prefix='/backend')
 
 
 class TimelineForm(FlaskForm):
     timeline_title = StringField(u'名称')
+    timeline_content = TextAreaField(u'时间线内容')
+    submit = SubmitField(u'添加时间线')
+
 
 @add_timeline_bp.route('/addTimeline')
 def index():
-    return render_template('/backend/addTimeline.html')
+    form = TimelineForm()
+    if form.validate_on_submit():
+        timeline_title = form.timeline_title
+        timeline_content = form.timeline_content
+    return render_template('/backend/addTimeline.html', form=form)
