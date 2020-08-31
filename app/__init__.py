9cfb0069fd006ef9535abe897bccf602184ce91c
jiangwei1 <jiangwei1@kylinos.com>
2020-08-31 10:01:55 +0800

    update config

--- app/__init__.py
+++ app/__init__.py
@@ -42,10 +42,10 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     app.config.from_mapping(test_config)
 
     try:
         os.makedirs(app.instance_path)
